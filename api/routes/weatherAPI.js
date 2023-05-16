0ff33591b699999a5faa046d9c2d193b47fd6c27
William <williamw15@protonmail.com>
2023-05-16 18:18:34 -0400

    add CORS package and enable requests only from front-end application

--- api/routes/weatherAPI.js
+++ api/routes/weatherAPI.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
+const app = express();
+
+/** Allow requests only from the front-end application. */
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
+
+/** Apply CORS middleware with options. */
+router.use(cors(corsOptions));
 
 /** Get weather API */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   // Add API key from openweathermap here as a string
-  res.send('');
+  res.send('c4b8c76df5bc9949fc6e2c820f59a8be');
 });
+
+app.use('/', router);
 
 module.exports = router;
